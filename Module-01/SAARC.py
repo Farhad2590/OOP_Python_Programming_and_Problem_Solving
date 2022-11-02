708f024ac7fe2fdd6950f6184eb47ae25b6d63cd
Farhad2590 <farhadhossen2590@gmail.com>
2022-11-03 00:59:01 +0600

    Update SAARC.py

--- Module-01/SAARC.py
+++ Module-01/SAARC.py
@@ -1,3 +1,14 @@
 saarc = ["Bangladesh", "Afganistan", "Bhutan", "Nepal", "India", "Pakistan", "Srilanka"]
 
-print(saarc)
+# print(saarc)
+# print(saarc[0])
+
+# print("Number of countries in saarc:",len(saarc))
+
+# "Bangladesh" in saarc
+
+country = input("Enter the name of the country: ")
+if country in saarc:
+    print(country, "is a member of SAARC")
+else:
+    print(country, "is not a member of SAARC")
